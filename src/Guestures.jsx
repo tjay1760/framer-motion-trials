89732fc6f7683c52036f92356bc2ca98fe8ba567
tjay1760 <mcjthiongo@gmail.com>
2025-10-03 17:22:13 +0300

    tap

--- src/Guestures.jsx
+++ src/Guestures.jsx
@@ -6,7 +6,7 @@ const Guestures = () => {
     <div className="min-h-80 min-w-80">
       <motion.button className="rounded p-2 bg-blue-700"
       whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.4 }}
+      whileTap={{ scale: 0.9 }}
       >
         Move!
       </motion.button>
