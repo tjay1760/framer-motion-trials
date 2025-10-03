5c72ada1da4789abc9ca1a6cf517cecd77381cc0
tjay1760 <mcjthiongo@gmail.com>
2025-10-03 18:52:54 +0300

    motion config

--- src/Guestures.jsx
+++ src/Guestures.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { motion } from "motion/react";
+import { motion, MotionConfig } from "motion/react";
 
 const Guestures = () => {
   return (
-    <div className="min-h-80 min-w-80">
-      <motion.button className="rounded p-2 bg-blue-700"
+    <div className="min-h-80 min-w-80 ">
+      <MotionConfig transition={{ duration: 0.1, ease: "easeInOut" }}>
+      <motion.button className="rounded p-2 bg-blue-700 m-4"
       whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
+      whileTap={{ scale: 0.9, rotate: 20 }}
+      
       >
         Move!
       </motion.button>
+      <motion.button className="rounded p-2 bg-blue-700"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9, rotate: 20 }}
+     
+      style={{ backgroundColor:'red'}}
+      >
+        Move!
+      </motion.button>
+      </MotionConfig>
+
     </div>
   );
 };
